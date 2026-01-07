4bdc75ee6418612e6129eb282735b0fe72dc38bf
Artem Minin <artemminin.03@mail.ru>
2026-01-07 23:12:36 +0300

    middlewares

--- src/user/controller.js
+++ src/user/controller.js
@@ -1,13 +1,42 @@
 const express = require("express");
 const router = express.Router();
-const { getAllArr } = require("./service");
+const { getAllArr, getById, postObj, updateArray, deleteObj } = require("./service");
 
 router.get("/", (req, res) => {
-  res.send(getAllArr());
+  res.json(getAllArr());
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const result = getById(id);
+  if (result) {
+    res.json(result);
+  } else {
+    res.status(404).json({ error: "Not found" });
+  }
 });
 
 router.post("/", (req, res) => {
-  res.send("Создать пользователя");
+  const { name, surname, email, pwd } = req.body;
+  const result = postObj(name, surname, email, pwd);
+  res.status(201).json(result);
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const obj = req.body;
+  const result = updateArray(id, obj);
+  if (result == null) {
+    res.status(404).json({ error: "Not found" });
+  } else {
+    res.status(200).json(result);
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const result = deleteObj(id);
+  res.status(200).json(result);
 });
 
 module.exports = router;
--- src/user/service.js
+++ src/user/service.js
@@ -11,4 +11,42 @@ function getAllArr() {
   return arr;
 }
 
-module.exports = { getAllArr };
+function getById(id) {
+  const arr = read();
+  const data = arr.find((el) => el.id == id);
+  return data;
+}
+
+function postObj(label, category, priority) {
+  const arr = read();
+  const id = label.toLowerCase().split(" ").join("");
+  arr.push({ id, label, category, priority });
+  fs.writeFileSync("./languages.json", JSON.stringify(arr));
+  return arr;
+}
+
+function updateArray(id, obj) {
+  const arr = read();
+  const index = arr.findIndex((el) => el.id == id.toLowerCase());
+  if (index == -1) {
+    return null;
+  } else {
+    arr[index] = { ...arr[index], ...obj };
+    fs.writeFileSync("./library.json", JSON.stringify(arr));
+    return arr;
+  }
+}
+
+function deleteObj(id) {
+  const arr = read();
+  const index = arr.findIndex((el) => el.id == id.toLowerCase());
+  if (index == -1) {
+    return null;
+  } else {
+    arr.splice(index, 1);
+    fs.writeFileSync("./library.json", JSON.stringify(arr));
+    return arr;
+  }
+}
+
+module.exports = { getAllArr, getById, postObj, updateArray, deleteObj };
